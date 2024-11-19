92e3c94becb8ae0adc8856c32cc20360dff2f79a
Andrey Efremov <50556416+AndreyRGW@users.noreply.github.com>
2024-11-20 00:01:48 +0300

    Disable "full_skim_negative" and "disable_flipping_filter"

--- scripts/skimmed_cfg_script.py
+++ scripts/skimmed_cfg_script.py
@@ -25,35 +25,36 @@ class SkimmedCFGScript(scripts.Script):
                     label="Skimming CFG Scale"
                 )
 
-            with gr.Row():
-                full_skim_negative = gr.Checkbox(
-                    label="Full Skim Negative",
-                    value=False
-                )
-                disable_flipping_filter = gr.Checkbox(
-                    label="Disable Flipping Filter",
-                    value=False
-                )
+            # with gr.Row():
+            #     full_skim_negative = gr.Checkbox(
+            #         label="Full Skim Negative",
+            #         value=False
+            #     )
+            #     disable_flipping_filter = gr.Checkbox(
+            #         label="Disable Flipping Filter",
+            #         value=False
+            #     )
 
         self.infotext_fields = [
             (enabled, "Skimmed CFG Enabled"),
             (skimming_scale, "Skimming CFG Scale"),
-            (full_skim_negative, "Full Skim Negative"),
-            (disable_flipping_filter, "Disable Flipping Filter"),
+            # (full_skim_negative, "Full Skim Negative"),
+            # (disable_flipping_filter, "Disable Flipping Filter"),
         ]
 
-        return [enabled, skimming_scale, full_skim_negative, disable_flipping_filter]
+        # return [enabled, skimming_scale, full_skim_negative, disable_flipping_filter]
+        return [enabled, skimming_scale]
 
     def process(self, p, enabled, skimming_scale, full_skim_negative, disable_flipping_filter):
         shared.opts.data.pop("skimmed_cfg_enabled", None)
         shared.opts.data.pop("skimming_scale", None)
-        shared.opts.data.pop("full_skim_negative", None)
-        shared.opts.data.pop("disable_flipping_filter", None)
+        # shared.opts.data.pop("full_skim_negative", None)
+        # shared.opts.data.pop("disable_flipping_filter", None)
 
         if not enabled:
             return
 
         shared.opts.data["skimmed_cfg_enabled"] = enabled
         shared.opts.data["skimming_scale"] = skimming_scale
-        shared.opts.data["full_skim_negative"] = full_skim_negative
-        shared.opts.data["disable_flipping_filter"] = disable_flipping_filter
+        # shared.opts.data["full_skim_negative"] = full_skim_negative
+        # shared.opts.data["disable_flipping_filter"] = disable_flipping_filter
